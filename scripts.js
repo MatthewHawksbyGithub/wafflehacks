901e539bbf25f9022c4933ad1b4c3ba041c28354
Matthew Hawksby <matthewhawksby91@gmail.com>
2023-06-23 20:27:38 -0700

    commit message

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+function showText() {
+    var textBoxValue = ReadAbilityAI.getElementById("myTextBox").value;
+    var displayBox = ReadAbilityAI.getElementById("displayText");
+    displayBox.innerHTML = textBoxValue;
+  }
