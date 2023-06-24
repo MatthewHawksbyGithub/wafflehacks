6522022c9172a989bc12d8ad7826df690cff7437
Matthew Hawksby <matthewhawksby91@gmail.com>
2023-06-23 23:47:52 -0700

    begin adding javascript to the page, API functions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,54 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
+
+
+const configuration =  new Configuration({
+  organization: "org-mtnx3paq9vdUscGbK1znT4p4",
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post("/", async (req, res) => {
+
+  const { message } = req.body;
+
+  //Messages can be collected here in an array to create a history if needed.
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user", content: '${message}'},
+    ]
+  })
+
+  res.json({
+    completion: completion.data.choices[0].message
+  })
+
+});
+
+app.listen(port, () => (
+  console.log("example app listening")
+))
+
+
+
+
+/*
 function showText() {
     var textBoxValue = ReadAbilityAI.getElementById("myTextBox").value;
     var displayBox = ReadAbilityAI.getElementById("displayText");
     displayBox.innerHTML = textBoxValue;
   }
+*/
